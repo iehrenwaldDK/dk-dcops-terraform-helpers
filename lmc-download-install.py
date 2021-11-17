417c8492317c620768e1b2ed6ae8fb503c271edf
Ian Ehrenwald <iehrenwald@draftkings.com>
2021-11-17 09:36:13 -0500

    Finalizing changes for enabling collector primary/backup pairs

--- lmc-download-install.py
+++ lmc-download-install.py
@@ -18,94 +18,101 @@ if os.geteuid() != 0:
     sys.exit("You need to be root to run this.\n")
 
 COMPANY_SET = False
-COLLECTOR_ID = ''
-COLLECTOR_VERSION = ''
-COLLECTOR_SIZE = 'medium'
-COLLECTOR_EA = 'false'
-COLLECTOR_OSANDARCH = 'Linux64'
-COLLECTOR_GROUP_ID = ''
-COLLECTOR_GROUP_AB = 'false'
-ENABLE_FAILOVER = 'false'
-ESCALATION_CHAIN = ''
 SKIP_DOWNLOAD = False
 SKIP_INSTALL = False
+REALLY_SLEEP = True
+
+COLLECTOR_EA = 'false'
+COLLECTOR_ESC_CHAIN = ''
+COLLECTOR_FAILOVER = 'true'
+COLLECTOR_GROUP_ID = ''
+COLLECTOR_ID = ''
+COLLECTOR_OSANDARCH = 'Linux64'
+COLLECTOR_SIZE = 'medium'
+COLLECTOR_VERSION = ''
+
+COLLECTOR_GROUP_AB = 'false'
+
 SNMP_SECURITY = ''
 SNMP_AUTH = ''
 SNMP_AUTH_TOKEN = ''
 SNMP_PRIV = ''
 SNMP_PRIV_TOKEN = ''
 
-configuration = logicmonitor_sdk.Configuration()
+
+LM_API_CFG = logicmonitor_sdk.Configuration()
 
 try:
     options, arguments = getopt.getopt(sys.argv[1:], "",
-                       ['access-id=', 'access-key=', 'company=', 'collector-id=',
-                    'collector-size=', 'collector-version=', 'collector-ea=',
-                    'escalation-chain=', 'collector-group-ab=',
-                    'collector-group-id=', 'enable-failover='
-                    'skip-download', 'skip-install',
-                    'snmp-security=', 'snmp-auth-token=', 'snmp-auth=',
-                    'snmp-priv-token=', 'snmp-priv='])
+                                       ['access-id=', 'access-key=', 'company=',
+                                        'collector-id=', 'collector-ea=',
+                                        'collector-esc-chain=', 'collector-failover=',
+                                        'collector-group-id=', 'collector-os-and-arch=',
+                                        'collector-size=', 'collector-version=', 'collector-group-ab=',
+                                        'snmp-security=', 'snmp-auth=', 'snmp-auth-token=',
+                                        'snmp-priv=', 'snmp-priv-token=',
+                                        'skip-download', 'skip-install'])
+
 except getopt.GetoptError as e:
     print(f"{e}")
     sys.exit(2)
 for opt, arg in options:
-    if opt in ('--company'):
-        configuration.company = arg
+    print(f"{opt}={arg}\n")
+    if opt in '--company':
+        LM_API_CFG.company = arg
         COMPANY_SET = True
-    elif opt in ('--access-id'):
-        configuration.access_id = arg
-    elif opt in ('--access-key'):
-        configuration.access_key = arg
+    elif opt in '--access-id':
+        LM_API_CFG.access_id = arg
+    elif opt in '--access-key':
+        LM_API_CFG.access_key = arg
 
-    elif opt in ('--collector-id'):
+    elif opt in '--collector-id':
         COLLECTOR_ID = arg
-    elif opt in ('--collector-version'):
+    elif opt in '--collector-version':
         COLLECTOR_VERSION = arg
-    elif opt in ('--collector-size'):
+    elif opt in '--collector-size':
         COLLECTOR_SIZE = arg
-    elif opt in ('--collector-ea'):
+    elif opt in '--collector-ea':
         COLLECTOR_EA = arg
 
-    elif opt in ('--escalation-chain'):
-        ESCALATION_CHAIN = arg
-    elif opt in ('--collector-group-ab'):
+    elif opt in '--collector-esc-chain':
+        COLLECTOR_ESC_CHAIN = arg
+    elif opt in '--collector-group-ab':
         COLLECTOR_GROUP_AB = arg
-    elif opt in ('--collector-group-id'):
+    elif opt in '--collector-group-id':
         COLLECTOR_GROUP_ID = arg
 
-    elif opt in ('--skip-download'):
+    elif opt in '--skip-download':
         SKIP_DOWNLOAD = True
-    elif opt in ('--skip-install'):
+    elif opt in '--skip-install':
         SKIP_INSTALL = True
 
-    elif opt in ('--snmp-security'):
+    elif opt in '--snmp-security':
         SNMP_SECURITY = arg
-    elif opt in ('--snmp-auth'):
+    elif opt in '--snmp-auth':
         SNMP_AUTH = arg
-    elif opt in ('--snmp-priv'):
+    elif opt in '--snmp-priv':
         SNMP_PRIV = arg
-    elif opt in ('--snmp-auth-token'):
+    elif opt in '--snmp-auth-token':
         SNMP_AUTH_TOKEN = arg
-    elif opt in ('--snmp-priv-token'):
+    elif opt in '--snmp-priv-token':
         SNMP_PRIV_TOKEN = arg
 
-    elif opt in ('--enable-failover'):
-        ENABLE_FAILOVER = True
+    elif opt in '--collector-failover':
+        COLLECTOR_FAILOVER = arg
 
-if not configuration.access_id or \
-   not configuration.access_key or \
+if not LM_API_CFG.access_id or \
+   not LM_API_CFG.access_key or \
    not COMPANY_SET or \
    not COLLECTOR_ID:
     print("Not all required info is present, exiting")
     sys.exit(1)
 
-api_instance = logicmonitor_sdk.LMApi(
-    logicmonitor_sdk.ApiClient(configuration))
+lm_api = logicmonitor_sdk.LMApi(logicmonitor_sdk.ApiClient(LM_API_CFG))
 
 # Create and download the installer binary for the collector
 try:
-    GCI_response = api_instance.get_collector_installer(
+    GCI_response = lm_api.get_collector_installer(
         collector_id=COLLECTOR_ID,
         os_and_arch=COLLECTOR_OSANDARCH,
         collector_size=COLLECTOR_SIZE,
@@ -142,12 +149,12 @@ else:
 
 # If there was an escalation chain ID passed to the script, set the collector
 #  to use it
-if ESCALATION_CHAIN:
+if COLLECTOR_ESC_CHAIN:
     print("Finding requested collector-down escalation chain")
-    escalation_chain_filter = 'name:"' + ESCALATION_CHAIN + '"'
+    escalation_chain_filter = 'name:"' + COLLECTOR_ESC_CHAIN + '"'
 
     try:
-        GECL_response = api_instance.get_escalation_chain_list(
+        GECL_response = lm_api.get_escalation_chain_list(
             filter=escalation_chain_filter)
     except ApiException as e:
         print(f"Exception when calling LMApi->getEscalationChainList: {e}\n")
@@ -161,7 +168,7 @@ if ESCALATION_CHAIN:
 
     print("Retrieving new collector information")
     try:
-        GCBID_response = api_instance.get_collector_by_id(id=COLLECTOR_ID)
+        GCBID_response = lm_api.get_collector_by_id(id=COLLECTOR_ID)
     except ApiException as e:
         print(f"Exception when calling LMApi->getCollectorById: {e}\n")
 
@@ -170,7 +177,7 @@ if ESCALATION_CHAIN:
 
     print("Setting escalation chain ID on new collector")
     try:
-        PCBID_response = api_instance.patch_collector_by_id(
+        PCBID_response = lm_api.patch_collector_by_id(
             id=COLLECTOR_ID,
             body=updated_data)
     except ApiException as e:
@@ -178,10 +185,10 @@ if ESCALATION_CHAIN:
 
 # If collector group auto-balancing was enabled through the script arguments,
 #  enable it
-if COLLECTOR_GROUP_AB:
+if COLLECTOR_GROUP_AB == "true":
     print(f"Finding collector group id {GCBID_response.collector_group_id} for autobalancing")
     try:
-        GCGBID_response = api_instance.get_collector_group_by_id(GCBID_response.collector_group_id)
+        GCGBID_response = lm_api.get_collector_group_by_id(GCBID_response.collector_group_id)
     except ApiException as e:
         print(f"Exception when calling LMApi->get_collector_group_by_id: {e}\n")
 
@@ -191,9 +198,9 @@ if COLLECTOR_GROUP_AB:
 
     print("    Enabling auto-balancing on collector group")
     try:
-        PCGBID_response = api_instance.patch_collector_group_by_id(
-          id=GCBID_response.collector_group_id,
-          body=updated_data)
+        PCGBID_response = lm_api.patch_collector_group_by_id(
+            id=GCBID_response.collector_group_id,
+            body=updated_data)
     except ApiException as e:
         print(f"Exception when calling LMApi->patch_collector_group_by_id: {e}\n")
 
@@ -201,7 +208,7 @@ if COLLECTOR_GROUP_AB:
 #  default of 127.0.0.1_collector_id
 print("Setting correct hostname on collector device within LM")
 try:
-    GDBID_response = api_instance.get_device_by_id(GCBID_response.collector_device_id)
+    GDBID_response = lm_api.get_device_by_id(GCBID_response.collector_device_id)
 except ApiException as e:
     print(f"Exception when calling LMApi->getDeviceById: {e}\n")
 
@@ -209,7 +216,7 @@ updated_data = GDBID_response
 updated_data.display_name = GCBID_response.hostname
 
 try:
-    PD_response = api_instance.patch_device(
+    PD_response = lm_api.patch_device(
         id=GCBID_response.collector_device_id,
         body=updated_data)
 except ApiException as e:
@@ -226,14 +233,14 @@ if SNMP_SECURITY and SNMP_AUTH and SNMP_AUTH_TOKEN and SNMP_PRIV and SNMP_PRIV_T
     updated_data.custom_properties.append({'name':'snmp.privToken', 'value': SNMP_PRIV_TOKEN})
 
     try:
-        PD_response = api_instance.patch_device(
+        PD_response = lm_api.patch_device(
                         id=GCBID_response.collector_device_id,
                         body=updated_data,
                         op_type='replace')
     except ApiException as e:
         print(f"Exception when calling LMApi->patchDevice: {e}\n")
 
-if ENABLE_FAILOVER:
+if COLLECTOR_FAILOVER == "true":
     if not COLLECTOR_GROUP_ID:
         print("Collector failover setup requested but no collector group id provided, aborting")
         sys.exit(1)
@@ -241,16 +248,17 @@ if ENABLE_FAILOVER:
     # Why are we sleeping a random time?  So we give all collectors a chance
     #  to download/install/configure/verify.  It's safe for this to run on
     #  all collectors.
-    SLEEP_MIN=120
-    SLEEP_MAX=300
-    sleep_time = randint(SLEEP_MIN,SLEEP_MAX)
+    SLEEP_MIN = 120
+    SLEEP_MAX = 300
+    sleep_time = randint(SLEEP_MIN, SLEEP_MAX)
     print(f"Sleeping {sleep_time} seconds and then beginning failover setup")
-    sleep(sleep_time)
+    if REALLY_SLEEP:
+        sleep(sleep_time)
 
     print(f">> Finding collectors in collector group '{COLLECTOR_GROUP_ID}'")
     try:
         GCL_filter = 'collectorGroupId:"' + str(COLLECTOR_GROUP_ID) + '"'
-        GCL_response = api_instance.get_collector_list(
+        GCL_response = lm_api.get_collector_list(
             fields="id,backupAgentId,enableFailBack,enableFailOverOnCollectorDevice",
             filter=GCL_filter)
     except ApiException as e:
@@ -267,7 +275,7 @@ if ENABLE_FAILOVER:
     MY_INDEX = 0
     updated_data = GCL_response.items
     # This is a little tricky
-    for index,element in enumerate(updated_data):
+    for index, element in enumerate(updated_data):
         if index+1 >= GCL_response.total:
             MY_INDEX = 0
         else:
@@ -280,7 +288,7 @@ if ENABLE_FAILOVER:
         updated_data[index].enable_fail_over_on_collector_device = False
 
         try:
-            PCBID_response = api_instance.patch_collector_by_id(
+            PCBID_response = lm_api.patch_collector_by_id(
                 id=GCL_response.items[index].id,
                 body=updated_data[index])
         except ApiException as e:
@@ -288,7 +296,7 @@ if ENABLE_FAILOVER:
 
         print(">>> Verifying")
         try:
-            GCBID_response = api_instance.get_collector_by_id(
+            GCBID_response = lm_api.get_collector_by_id(
                 id=GCL_response.items[index].id,
                 fields="id,backupAgentId,enableFailBack,enableFailOverOnCollectorDevice")
         except ApiException as e:
@@ -298,10 +306,10 @@ if ENABLE_FAILOVER:
           GCBID_response.enable_fail_back is not True or \
           GCBID_response.enable_fail_over_on_collector_device is not False:
             print(">>>> Failed, received values: "
-                   f"backup_agent_id={GCBID_response.backup_agent_id} "
-                   f"enable_fail_back={GCBID_response.enable_fail_back} "
-                   f"enable_fail_over_on_collector_device="
-                   f"{GCBID_response.enable_fail_over_on_collector_device}")
+                  f"backup_agent_id={GCBID_response.backup_agent_id} "
+                  f"enable_fail_back={GCBID_response.enable_fail_back} "
+                  f"enable_fail_over_on_collector_device="
+                  f"{GCBID_response.enable_fail_over_on_collector_device}")
             sys.exit(1)
         else:
             print(">>>> Success")
